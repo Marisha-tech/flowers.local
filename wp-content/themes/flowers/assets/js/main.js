bcdeb8bb6c1d6fcb06648412ee92211793bd93d0
Marisha-tech <malayamarisha@yandex.ru>
2020-06-26 17:22:52 +0700

    Scroll total

--- wp-content/themes/flowers/assets/js/main.js
+++ wp-content/themes/flowers/assets/js/main.js
@@ -1,7 +1,31 @@
 /*Для галереи*/
 $(function () {
-    baguetteBox.run('.gallery');
-})
+    // baguetteBox.run('.gallery');
+    baguetteBox.run('.wp-block-gallery');/*Для галереи*/
+
+    /*Для прелоадера*/
+    $(window).on('load', function () {
+        $('.preloader').delay(500).fadeOut('slow', function () {
+            $(this).attr('style', 'display:none !important');
+        });
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300)
+        {
+            $('.scrollToTop').fadeIn();
+        }
+        else
+        {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    /*Для прокрутки*/
+    $('.scrollToTop').click(function () {
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+});
 /*Привязка картинки кнопки к youtube*/
 function onYouTubeIframeAPIReady() {
     var iStatus;
