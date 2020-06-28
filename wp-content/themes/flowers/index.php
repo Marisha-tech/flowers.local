0652ca69839440ee1b8f9682d665b82299d0e13b
Marisha-tech <51979512+Marisha-tech@users.noreply.github.com>
2020-06-28 17:10:57 +0700

    Page komnatnye-rasteniya

--- wp-content/themes/flowers/index.php
+++ wp-content/themes/flowers/index.php
@@ -1,57 +1,96 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package flowers
- */
+<? get_header();?>
 
-get_header();
-?>
+<section class="section-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="row">
+                    <? if (have_posts()) : ?>
+                        <? while (have_posts()) : the_post(); ?>
+                        <div class="col-md-6">
+                            <article class="article-preview">
+                                <div class="article-excerpt">
+                                    <?if (has_post_thumbnail()): ?>
+                                        <p>
+                                            <a href="<?the_permalink() ?>">
+                                                <?the_post_thumbnail('medium', array('class'=>'thumb')) ?>
+                                                <h4><?the_title();?></h4>
+                                            </a>
+                                            <?the_content('')?>
+                                        </p>
+                                    <?endif;?>
+                                    <p><a href="<?the_permalink() ?>" class="more"><?_e('Подробнее', 'flowers')?></a></p>
+                                </div>
+                            </article>
+                            <!-- /.article-preview -->
+                        </div>
+                        <? endwhile; ?>
+                        <?the_posts_pagination(array(
+                            'end_size'     => 1,
+                            'mid_size'     => 1,
+                            'type'         => 'list',
+                        ))?>
+                    <?endif;?>
+                </div>
 
-	<main id="primary" class="site-main">
+                <!--<nav aria-label="...">
+                    <ul class="pagination">
+                        <li class="page-item disabled">
+                            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item active" aria-current="page">
+                            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>-->
+            </div>
+            <?get_sidebar()?>
+            <!-- /.col-md-4 -->
+            <!-- /.col-md-8 -->
+<!--            <div class="col-md-4">-->
+<!--                <div class="sidebar-widget widget-categories">-->
+<!--                    <h5 class="widget-title">Рубрики</h5>-->
+<!--                    <h5 class="widget-title">Комнатные растения</h5>-->
+<!--                    <ul>-->
+<!--                        <li>-->
+<!--                            <a href="#"><img src="img/i/1.png" alt="" class="category_icon">Ампельные растения</a>-->
+<!--                            <span class="cat-count">42</span>-->
+<!--                        </li>-->
+<!--                        <li>-->
+<!--                            <a href="#"><img src="img/i/2.png" alt="" class="category_icon">Декоративно-лиственные</a>-->
+<!--                            <span class="cat-count">42</span>-->
+<!--                        </li>-->
+<!--                        <li>-->
+<!--                            <a href="#"><img src="img/i/3.png" alt="" class="category_icon">Орхидеи</a>-->
+<!--                            <span class="cat-count">42</span>-->
+<!--                        </li>-->
+<!--                        <li>-->
+<!--                            <a href="#"><img src="img/i/4.png" alt="" class="category_icon">Пальмы</a>-->
+<!--                            <span class="cat-count">42</span>-->
+<!--                        </li>-->
+<!--                        <li>-->
+<!--                            <a href="#"><img src="img/i/5.png" alt="" class="category_icon">Суккуленты</a>-->
+<!--                            <span class="cat-count">42</span>-->
+<!--                        </li>-->
+<!--                        <li>-->
+<!--                            <a href="#"><img src="img/i/6.png" alt="" class="category_icon">Цветущие</a>-->
+<!--                            <span class="cat-count">42</span>-->
+<!--                        </li>-->
+<!---->
+<!---->
+<!--                    </ul>-->
+<!--                </div>-->
+<!--                <!-- /.sidebar-widget widget-categories -->
+<!--            </div>-->
+            <!-- /.col-md-4 -->
+        </div>
+    </div>
+</section>
+<!-- /.section-content -->
 
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<? get_footer();?>
