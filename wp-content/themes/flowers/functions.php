73c4143305f39460fd8f97cb2482a1903185a7d3
Marisha-tech <malayamarisha@yandex.ru>
2020-07-03 17:21:27 +0700

    Function comment

--- wp-content/themes/flowers/functions.php
+++ wp-content/themes/flowers/functions.php
@@ -292,4 +292,74 @@ function exclude_category_home  ( $query ) {
 
 add_filter ( 'pre_get_posts', 'exclude_category_home' );
 
+//Комментарии
+
+function flowers_comment( $comment, $args, $depth ) {
+    if ( 'div' === $args['style'] ) {
+        $tag       = 'div';
+        $add_below = 'comment';
+    } else {
+        $tag       = 'li';
+        $add_below = 'div-comment';
+    }
+
+    $classes = ' ' . comment_class( empty( $args['has_children'] ) ? '' : 'parent', null, null, false );
+    ?>
+
+    <<?php echo $tag, $classes; ?> id="comment-<?php comment_ID() ?>">
+    <?php if ( 'div' != $args['style'] ) { ?>
+        <div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
+    } ?>
+
+    <div class="comment-author vcard">
+        <?php
+        if ( $args['avatar_size'] != 0 ) {
+            echo get_avatar( $comment, $args['avatar_size'] );
+        }
+        printf(
+            __( '<b class="fn fn-padding">%s</b> <span class="comment-date">'.get_comment_date().'NTCN</span>' ),
+            get_comment_author_link()
+        );
+        ?>
+    </div>
+
+    <?php if ( $comment->comment_approved == '0' ) { ?>
+        <em class="comment-awaiting-moderation">
+            <?php _e( 'Your comment is awaiting moderation.' ); ?>
+        </em><br/>
+    <?php } ?>
+
+    <!--<div class="comment-meta commentmetadata">
+        <a href="<?php /*echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); */?>">
+            <?php
+/*            printf(
+                __( '%1$s at %2$s' ),
+                get_comment_date(),
+                get_comment_time()
+            ); */?>
+        </a>
+
+        <?php /*edit_comment_link( __( '(Edit)' ), '  ', '' ); */?>
+    </div>-->
+
+    <?php comment_text(); ?>
+
+    <div class="reply">
+        <?php
+        comment_reply_link(
+            array_merge(
+                $args,
+                array(
+                    'add_below' => $add_below,
+                    'depth'     => $depth,
+                    'max_depth' => $args['max_depth']
+                )
+            )
+        ); ?>
+    </div>
+
+    <?php if ( 'div' != $args['style'] ) { ?>
+        </div>
+    <?php }
+}
 
